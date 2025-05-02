0324c0cdc511851d1ed8e6f907615f84fe75021a
jashanpratapsingh <jashanpratap123@gmail.com>
2025-05-01 22:51:59 -0400

    changed the supabase in this case

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -8,7 +8,13 @@ if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase URL or Anonymous Key')
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true
+  }
+})
 
 export type AuthError = {
   message: string
